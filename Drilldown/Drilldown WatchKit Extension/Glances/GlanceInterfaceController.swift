614616d99caecc519750afa33f1df3b5487a1c0d
Pavel Hamrik <pavel.hamrik@gmail.com>
2016-01-14 19:08:56 +0100

    Glance attempt.

---
+++ Drilldown/Drilldown WatchKit Extension/Glances/GlanceInterfaceController.swift
@@ -0,0 +1,47 @@
+//
+//  GlanceInterfaceController.swift
+//  Drilldown
+//
+//  Created by Pavel Hamřík on 14.01.16.
+//  Copyright © 2016 Pavel Hamřík. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceInterfaceController: WKInterfaceController {
+    
+    
+    @IBOutlet var titleLabel: WKInterfaceLabel!
+    
+    //@IBOutlet var sourceLabel: WKInterfaceLabel!
+    
+    @IBOutlet var timeLabel: WKInterfaceLabel!
+    
+
+    override func awakeWithContext(context: AnyObject?) {
+        
+        super.awakeWithContext(context)
+        
+    }
+
+    
+    override func willActivate() {
+        
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        
+        let articles = Article.allArticles()
+        titleLabel.setText(articles.first?.title)
+        
+    }
+
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    
+}
